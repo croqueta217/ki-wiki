947c66e8402306a0c45309754574d274f43a6c28
croqueta217 <croqueta217@users.noreply.github.com>
2025-10-30 17:53:55 +0000

    deploy: d3885b18306f3d992435bfad2f71a56efb854c68

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Seite 1",
-        "excerpt":"Das ist Seite 1 im Wiki.  ","categories": [],
+        "excerpt":"Das ist Seite 1 im Wiki.   bla  ","categories": [],
         "tags": [],
         "url": "/jekyll-wiki-template/seite1/",
         "teaser": null
